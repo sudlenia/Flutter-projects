4ce51cf72d05681e7d504daa76039c8258943537
sudlenia <sudlenia@gmail.com>
2024-05-08 17:58:12 +0700

    Task 56

---
+++ 56/lib/my_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class MyApp extends StatelessWidget {
+  
+const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Lottie.asset('assets/Animation - 1712719366652.json'),
+    );
+  }
+}
